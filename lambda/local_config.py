fb95432ecb8333f45d0d1b5f7c40b3e0a9b7da4c
Matt McFarland <matmcfarland@gmail.com>
2016-08-09 15:20:41 -0500

    update config with user and docs about region

--- lambda/local_config.py
+++ lambda/local_config.py
@@ -1,5 +1,4 @@
-# I expect you to replace this file with your local config
-REGION= 'us-west-2' # Change to region your AWS Lambda function is in
 CHEF_SERVER_URL = 'https://my_server/organizations/thisorg'
-USERNAME = 'username'
+REGION= 'us-east-1' # This should be the region the KMS key is stored in
+USERNAME = 'busboy'
 VERIFY_SSL = False
