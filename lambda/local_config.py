4babbd20cb1afcc3a803c116a80e6bfdfec96ecb
Matt McFarland <matmcfarland@gmail.com>
2016-11-21 18:48:47 -0600

    updates for new region

--- lambda/local_config.py
+++ lambda/local_config.py
@@ -1,4 +1,4 @@
 CHEF_SERVER_URL = 'https://my_server/organizations/thisorg'
-REGION= 'us-east-1' # This should be the region the KMS key is stored in
+REGION= 'us-west-2' # This should be the region the KMS key is stored in
 USERNAME = 'busboy'
 VERIFY_SSL = False
